d93843c9b4da754321673e3d85c4faa8c37fd0e7
mohammadK03 <kazemim1118@gmail.com>
2023-05-02 00:34:52 +0430

    test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   console.log('hi')
 
-  'i also added this one to test'
+  'i also added this one to test '
 
   return (
     <div>this is main page rfefrerf</div>
